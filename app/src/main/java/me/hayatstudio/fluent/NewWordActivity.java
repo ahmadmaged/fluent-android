f44e9654a2af91d91a22e5d1789d140ae64dc5a7
Ahmad Maged <59603497+ahmadmaged@users.noreply.github.com>
2021-09-14 10:15:32 +0200

    feature: classify words as nouns, verbs or other

--- app/src/main/java/me/hayatstudio/fluent/NewWordActivity.java
+++ app/src/main/java/me/hayatstudio/fluent/NewWordActivity.java
@@ -1,16 +1,36 @@
 package me.hayatstudio.fluent;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
 
 public class NewWordActivity
     extends AppCompatActivity
 {
+    private MaterialButton nounButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_word);
+
+        nounButton = findViewById(R.id.nounButton);
+        nounButton.setChecked(true);
+    }
+
+    public void nounButtonAction(View view)
+    {
+    }
+
+    public void verbButtonAction(View view)
+    {
+    }
+
+    public void otherButtonAction(View view)
+    {
     }
 }
